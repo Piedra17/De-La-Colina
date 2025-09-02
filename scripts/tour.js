3cf173dacac54908515a974439b2a2f80037c8f7
Piedra17 <163149335+Piedra17@users.noreply.github.com>
2025-09-02 13:14:20 -0600

    Add files via upload

--- scripts/tour.js
+++ scripts/tour.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", function () {
     const animatedElements = document.querySelectorAll('.from-left, .from-right');
 
@@ -14,4 +13,40 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     animatedElements.forEach(el => observer.observe(el));
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuBtn = document.querySelector(".menu-btn");
+    const opciones = document.querySelector(".opciones");
+    const overlay = document.getElementById("menuOverlay");
+
+    menuBtn.addEventListener("click", function () {
+        opciones.classList.toggle("active");
+        overlay.classList.toggle("show");
+    });
+
+    overlay.addEventListener("click", function () {
+        opciones.classList.remove("active");
+        overlay.classList.remove("show");
+    });
+});
+
+
+const cards = document.querySelectorAll('.card');
+let currentIndex = 0;
+
+function focusCard(index) {
+  cards.forEach(card => card.classList.remove('active'));
+  cards[index].focus();
+  cards[index].classList.add('active');
+}
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'ArrowRight') {
+    currentIndex = (currentIndex + 1) % cards.length;
+    focusCard(currentIndex);
+  } else if (e.key === 'ArrowLeft') {
+    currentIndex = (currentIndex - 1 + cards.length) % cards.length;
+    focusCard(currentIndex);
+  }
 });
